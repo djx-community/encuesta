29493f91430bb28b4f05bffdf8a3356e261924b0
mohammedek <mohammedek159@gmail.com>
2022-10-23 12:29:05 +0530

    home screen finiched

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -11,33 +11,47 @@ import 'package:flutter/src/widgets/framework.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        bottomNavigationBar: navBars(),
-        body: Column(children: [
-          UserIndicator(
-              // bug in container
-              // if name = container size is fixed and looks bad
-              user_Name: 'mohammed ek',
-              user_Avatar:
-                  "https://c1.alamy.com/thumbs/rd50gg/avatar-sign-icon-vector-illustration-for-personal-and-commercial-use-clean-look-trendy-icon-rd50gg.jpg"),
-          Image.asset(homeScreenImage),
-          customButton(
-              onPress: Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const SplashScreen())),
-              button_text: "Quick play",
-              button_color: Colors.white54),
-          customButton(
-              onPress: Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const SplashScreen())),
-              button_text: 'Social Player',
-              button_color: PRIMARY_COLOR),
-        ]));
+    return MaterialApp(
+      home: Scaffold(
+          bottomNavigationBar: navBars(),
+          body: Column(children: [
+            UserIndicator(
+                // bug in container
+                // if name = container size is fixed and looks bad
+                user_Name: 'mohammed ek',
+                user_Avatar:
+                    "https://c1.alamy.com/thumbs/rd50gg/avatar-sign-icon-vector-illustration-for-personal-and-commercial-use-clean-look-trendy-icon-rd50gg.jpg"),
+            Image.asset(
+              homeScreenImage,
+              height: 260,
+              width: 320,
+            ),
+            customButton(
+              button_text: 'open game',
+              button_color: Colors.purple,
+              onPress: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => WinningScreen()));
+              },
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            customButton(
+              button_text: 'single play',
+              button_color: Colors.indigo,
+              onPress: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => SplashScreen()));
+              },
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            customButton(button_text: 'Quick Play', button_color: Colors.red)
+          ])),
+    );
   }
 }
