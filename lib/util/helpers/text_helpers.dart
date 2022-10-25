40bbe9ea2826398e02911dd406fe56db35d24d1e
Mubashir Angathil <mubashirangathil5142@gmail.com>
2022-10-25 21:25:14 +0530

    theme.dart,main.dart,text_helpers.dart updated

--- lib/util/helpers/text_helpers.dart
+++ lib/util/helpers/text_helpers.dart
@@ -8,3 +8,14 @@ const negativeGreeting = 'Oops! You Lost!';
 const stayTuned = 'Stay Tuned...';
 const loading = "Loading...";
 const loadingValue = 0.5;
+const homeHelperText="Home";
+const newQuizHelperText="Take A New Quiz";
+const leaderBoardHelperText="Leader Board";
+
+//Leader Board Assets
+const profilePic1 = "https://media.istockphoto.com/photos/headshot-portrait-of-smiling-male-employee-in-office-picture-id1309328823?b=1&k=20&m=1309328823&s=170667a&w=0&h=a-f8vR5TDFnkMY5poQXfQhDSnK1iImIfgVTVpFZi_KU=";
+const profilePic2 ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnS1o3mO3S_Nkfw1WAGaRJ6KaOGgODpfoOsA&usqp=CAU";
+const profilePic3 ="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlciUyMHByb2ZpbGV8ZW58MHx8MHx8&w=1000&q=80";
+final users=['John','Emmy','Michel','user3','user4','user5','user6','user7','user8','user9','user10','user11'];
+final points=[100,90,80,70,40,40,30,30,20,10,10,5];
+ 
