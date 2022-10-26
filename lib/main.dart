03d001d52425966286b043e014f3e72ff4820ada
Mubashir Angathil <mubashirangathil5142@gmail.com>
2022-10-27 01:15:33 +0530

    custom_button.dart's file name changed

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:encuesta/config/themes/themes.dart';
 import 'package:encuesta/screens/failed_screen.dart';
 import 'package:encuesta/screens/game_loading_screen.dart';
 import 'package:encuesta/screens/home_screen.dart';
+import 'package:encuesta/screens/hosting_screen.dart';
 import 'package:encuesta/screens/leader_board_screen.dart';
 import 'package:encuesta/screens/splash_screen.dart';
 import 'package:encuesta/screens/winning_screen.dart';
@@ -23,10 +24,11 @@ class MyApp extends StatelessWidget {
             textTheme: Theme.of(context).textTheme.apply(fontFamily: 'Serif')),
         initialRoute: splashScreenRoute,
         routes: {
-          splashScreenRoute: (context) => const SplashScreen(),
+          splashScreenRoute: (context) =>const  SplashScreen(),
           homeScreenRoute: (context) => const HomeScreen(),
           winningScreenRoute: (context) => const WinningScreen(),
           failedScreenRoute: (context) => const FailedScreen(),
+          hostingScreenRoute:(context)=>const HostingScreen(),
           leaderBoardScreenRoute: (context) =>const  LeaderBoardScreen(),
           gameLoadingScreenRoute: (context) => const GameLoadingScreen(),
         });
