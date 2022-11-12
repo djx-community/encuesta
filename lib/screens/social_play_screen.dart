85ff52264d571e550a71a6dc2a5b8991c31da04b
Abdul Samad M J <samadmj4@gmail.com>
2022-11-12 21:33:49 +0530

    Social Play Screen Basic UI added

--- lib/screens/social_play_screen.dart
+++ lib/screens/social_play_screen.dart
@@ -1,37 +1,47 @@
 import 'package:encuesta/config/assets_path/assets_path.dart';
 import 'package:encuesta/config/routes/routes.dart';
 import 'package:encuesta/config/themes/themes.dart';
+import 'package:encuesta/screens/hosting_screen.dart';
 import 'package:encuesta/screens/winning_screen.dart';
 import 'package:encuesta/util/helpers/text_helpers.dart';
 import 'package:encuesta/widgets/custom_buttons_widgets.dart';
 import 'package:encuesta/widgets/nav_bar.dart';
-import 'package:encuesta/widgets/user_id_app_card_widget.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class SocialPlayScreen extends StatelessWidget {
+  const SocialPlayScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         bottomNavigationBar: const navBars(),
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          shadowColor: Colors.white,
+          leading: IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: const Icon(
+                Icons.arrow_back_ios,
+                color: Colors.black,
+              )),
+        ),
         body: SafeArea(
           child: Column(children: [
-            Container(
-                padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
-                child: const UserIdAppCardWidget()),
-            const Image(image: AssetImage(homeScreenImage)),
-            const Text(letsHackHelperText,
+            const Text(socialPlayHelperText,
                 style: TextStyle(fontSize: 30, fontWeight: FontWeight.w400)),
+            const Image(image: AssetImage(socialPlayImage)),
             const SizedBox(height: 20),
             CustomButton(
               onPress: () {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                      builder: (context) => const WinningScreen()),
+                      builder: (context) => const HostingScreen()),
                 );
               },
-              buttonText: quickPlayHelperText,
+              buttonText: hostRoomHelperText,
               buttonColor: PRIMARY_COLOR,
               buttonTextColor: textColor,
             ),
@@ -39,20 +49,10 @@ class HomeScreen extends StatelessWidget {
               height: 20,
             ),
             CustomOutlinedButton(
-              buttonText: socialPlayHelperText,
+              buttonText: joinRoomHelperText,
               buttonTextColor: textColor,
               onPress: () {
-                Navigator.pushNamed(context, socialPlayScreenRoute);
-              },
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            CustomOutlinedButton(
-              buttonText: singlePlayerHelperText,
-              buttonTextColor: textColor,
-              onPress: () {
-                Navigator.pushNamed(context, hostingScreenRoute);
+                Navigator.pushNamed(context, joinedRoomScreenRoute);
               },
             ),
           ]),
