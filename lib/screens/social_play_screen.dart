bec2341ba9b601441b69d770ed465d5ebd7ec5a7
Mubashir Angathil <mubashirangathil5142@gmail.com>
2022-11-13 21:22:34 +0530

    social play image flexible issue solved

--- lib/screens/social_play_screen.dart
+++ lib/screens/social_play_screen.dart
@@ -2,7 +2,6 @@ import 'package:encuesta/config/assets_path/assets_path.dart';
 import 'package:encuesta/config/routes/routes.dart';
 import 'package:encuesta/config/themes/themes.dart';
 import 'package:encuesta/screens/hosting_screen.dart';
-import 'package:encuesta/screens/winning_screen.dart';
 import 'package:encuesta/util/helpers/text_helpers.dart';
 import 'package:encuesta/widgets/custom_buttons_widgets.dart';
 import 'package:encuesta/widgets/nav_bar.dart';
@@ -17,7 +16,7 @@ class SocialPlayScreen extends StatelessWidget {
         bottomNavigationBar: const navBars(),
         appBar: AppBar(
           backgroundColor: Colors.white,
-          shadowColor: Colors.white,
+          shadowColor: Colors.transparent,
           leading: IconButton(
               onPressed: () {
                 Navigator.pop(context);
@@ -28,35 +27,47 @@ class SocialPlayScreen extends StatelessWidget {
               )),
         ),
         body: SafeArea(
-          child: Column(children: [
-            const Text(socialPlayHelperText,
-                style: TextStyle(fontSize: 30, fontWeight: FontWeight.w400)),
-            const Image(image: AssetImage(socialPlayImage)),
-            const SizedBox(height: 20),
-            CustomButton(
-              onPress: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const HostingScreen()),
-                );
-              },
-              buttonText: hostRoomHelperText,
-              buttonColor: PRIMARY_COLOR,
-              buttonTextColor: textColor,
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            CustomOutlinedButton(
-              buttonText: joinRoomHelperText,
-              buttonTextColor: textColor,
-              onPress: () {
-                Navigator.pushNamed(context,
-                    joinedRoomScreenRoute); //need to add popup for entering room creddentials
-              },
-            ),
-          ]),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                const Text(socialPlayHelperText,
+                    style:
+                        TextStyle(fontSize: 30, fontWeight: FontWeight.w400)),
+                const Center(
+                    child: Flexible(
+                        child: Image(
+                            image: AssetImage(socialPlayImage), width: 375))),
+                const SizedBox(height: 20),
+                Flexible(
+                  child: Column(
+                    children: [
+                      CustomButton(
+                        onPress: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const HostingScreen()),
+                          );
+                        },
+                        buttonText: hostRoomHelperText,
+                        buttonColor: PRIMARY_COLOR,
+                        buttonTextColor: textColor,
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      CustomOutlinedButton(
+                        buttonText: joinRoomHelperText,
+                        buttonTextColor: textColor,
+                        onPress: () {
+                          Navigator.pushNamed(context,
+                              joinedRoomScreenRoute); //need to add popup for entering room creddentials
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ]),
         ));
   }
 }
