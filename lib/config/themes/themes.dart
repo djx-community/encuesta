e5c05c40a091d1ca69685781cc48f72dc5a3ba7e
Mubashir Angathil <mubashirangathil5142@gmail.com>
2022-11-20 21:01:37 +0530

    theme.dart updated

--- lib/config/themes/themes.dart
+++ lib/config/themes/themes.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 const APP_COLOR = Colors.white;
 const PRIMARY_COLOR = Color.fromARGB(255, 224, 255, 254);
+const tabIndicatorColor=Color.fromARGB(255, 121, 255, 251);
 const textColor = Colors.black;
 const winningScreenColor = Color.fromARGB(255, 236, 250, 250);
 const progressionBarBgColor = Color.fromRGBO(187, 222, 251, 1);
@@ -10,3 +11,4 @@ const negativeButtonBorderColor = Colors.red;
 const positiveButtonBorderColor = Colors.green;
 const rightAnswerColor=Color.fromARGB(255, 13, 161, 18);
 const wrongAnswerColor=Color.fromARGB(255, 255, 0, 0);
+const selectedOptionColor=Color.fromARGB(255, 47, 162, 255);
