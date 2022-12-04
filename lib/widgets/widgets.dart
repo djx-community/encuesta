d0fd088e590f65a4954c691596168e952b976e3d
Mubashir Angathil <mubashirangathil5142@gmail.com>
2022-12-05 03:15:57 +0530

    Connection lost alert widget created

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,4 +1,6 @@
+import 'package:encuesta/config/themes/themes.dart';
 import 'package:encuesta/util/helpers/text_helpers.dart';
+import 'package:encuesta/widgets/custom_buttons_widgets.dart';
 import 'package:flutter/material.dart';
 
 // General SnackBar Widget
@@ -46,6 +48,62 @@ Future<void> generalAlertDialog(BuildContext context, String title,
               Navigator.pushNamed(context, route);
             },
           ),
+        ],
+      );
+    },
+  );
+}
+
+//Connection lost Alert Dialog
+Future<void> connectionLostAlertDialog(
+    BuildContext context, String title, String message) async {
+  return showDialog<void>(
+    context: context,
+    barrierDismissible: false, // user must tap button!
+    builder: (BuildContext context) {
+      return AlertDialog(
+        content: SingleChildScrollView(
+          child: ListBody(
+            children: <Widget>[
+              const Icon(Icons.warning_amber_rounded,
+                  color: Colors.amber, size: 100),
+              Center(
+                  child: Text(
+                title,
+                style: const TextStyle(fontSize: 25),
+              )),
+              const SizedBox(height: 10),
+              Center(
+                  child: Text(
+                message,
+                style: const TextStyle(fontSize: 20),
+              )),
+            ],
+          ),
+        ),
+        actions: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CustomButton(
+                buttonColor: tertiaryColor,
+                buttonText: cancelHelperText,
+                buttonTextColor: textColor,
+                buttonWidth: 100,
+                buttonHeight: 40,
+                onPress: () {Navigator.pop(context);},
+              ),
+              const SizedBox(width: 10),
+              CustomButton(
+                buttonColor: PRIMARY_COLOR,
+                buttonText: tryAgainHelperText,
+                buttonTextColor: textColor,
+                buttonWidth: 100,
+                buttonHeight: 40,
+                onPress: () {},
+              )
+            ],
+          )
         ],
       );
     },
