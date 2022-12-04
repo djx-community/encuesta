5a8f20ae417a03d2c4f915e89f078234062fb62d
Abdul Samad M J <samadmj4@gmail.com>
2022-12-04 15:32:17 +0530

    joined room redirection changed

--- lib/screens/joined_room_screen.dart
+++ lib/screens/joined_room_screen.dart
@@ -137,6 +137,6 @@ class _JoinedRoomScreenState extends State<JoinedRoomScreen> {
 
   Future<void> changeToQuizScreen(context) async {
     await Future<dynamic>.delayed(const Duration(seconds: 3));
-    Navigator.of(context).pushReplacementNamed(hostViewOfGameScreenRoute);
+    Navigator.of(context).pushReplacementNamed(quizScreenRoute);
   }
 }
