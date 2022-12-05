89416b1e564f819c9d52c11daf8c036c8ab524e9
Mubashir Angathil <mubashirangathil5142@gmail.com>
2022-12-05 23:17:57 +0530

    copy to clipboard functionality of icon click and long press on card completed

--- lib/widgets/user_id_app_card_widget.dart
+++ lib/widgets/user_id_app_card_widget.dart
@@ -3,6 +3,7 @@ import 'package:encuesta/util/helpers/text_helpers.dart';
 import 'package:encuesta/widgets/hosting_room_credential_edit_alert_widget.dart';
 import 'package:encuesta/widgets/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class UserIdAppCardWidget extends StatelessWidget {
   const UserIdAppCardWidget({super.key});
@@ -16,46 +17,54 @@ class UserIdAppCardWidget extends StatelessWidget {
       },
       child: SizedBox(
         width: 300,
-        child: Card(
-          color: PRIMARY_COLOR,
-          shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.horizontal(
-                  left: Radius.circular(30), right: Radius.circular(10))),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              const CircleAvatar(
-                radius: 25,
-                backgroundImage: NetworkImage(profilePic1),
-              ),
-              const Text(
-                userIDHelperText,
-                style: TextStyle(fontSize: 15),
-              ),
-              Row(
-                children: [
-                  IconButton(
-                    onPressed: () {
-                      generalSnackBar(context, "ID Copied 20202");
-                      // generalAlertDialog(context,alertHelperText,alertMessageHelperText,true);
-                    },
-                    icon: const Icon(
-                      Icons.copy,
-                      color: textColor,
+        child: GestureDetector(
+          onLongPress: () {
+            generalSnackBar(context, "Copied to Clipboard");
+            Clipboard.setData(const ClipboardData(text: userIDHelperText));
+          },
+          child: Card(
+            color: PRIMARY_COLOR,
+            shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.horizontal(
+                    left: Radius.circular(30), right: Radius.circular(10))),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                const CircleAvatar(
+                  radius: 25,
+                  backgroundImage: NetworkImage(profilePic1),
+                ),
+                const Text(
+                  userIDHelperText,
+                  style: TextStyle(fontSize: 15),
+                ),
+                Row(
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        generalSnackBar(context, "Copied to Clipboard");
+                        Clipboard.setData(
+                            const ClipboardData(text: userIDHelperText));
+                        // generalAlertDialog(context,alertHelperText,alertMessageHelperText,true);
+                      },
+                      icon: const Icon(
+                        Icons.copy,
+                        color: textColor,
+                      ),
+                      iconSize: 20,
                     ),
-                    iconSize: 20,
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(
-                      Icons.share,
-                      color: textColor,
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.share,
+                        color: textColor,
+                      ),
+                      iconSize: 20,
                     ),
-                    iconSize: 20,
-                  ),
-                ],
-              ),
-            ],
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
